894a2e47197d8aae29857caf4a435b24b8866e1a
Deric Walintukan <deric.walintukan@gmail.com>
2014-02-12 18:27:44 -0800

    trying to get swipebox to work

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Scrolling animation when clicking anchors
+var $root = $('html, body');
+$('a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top -50
+    }, 600);
+    return false;
+});
+
+// Initialize swipebox
+jQuery(function($) {
+	$(".swipebox").swipebox({ 
+		
+	});
+});
